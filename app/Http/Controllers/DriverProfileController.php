33e897881e354cfcc1de2aec484677969bcfccee
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-08 15:26:13 -0600

    Runs php artisan make:model DriverProfile --all

---
+++ app/Http/Controllers/DriverProfileController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDriverProfileRequest;
+use App\Http\Requests\UpdateDriverProfileRequest;
+use App\Models\DriverProfile;
+
+class DriverProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDriverProfileRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DriverProfile $driverProfile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DriverProfile $driverProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDriverProfileRequest $request, DriverProfile $driverProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DriverProfile $driverProfile)
+    {
+        //
+    }
+}
