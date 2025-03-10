1f209a7636761f5cd0b95606a7892294c214dbed
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-09 21:29:27 -0600

    Driver migration

--- database/migrations/2025_03_08_212306_create_driver_profiles_table.php
+++ database/migrations/2025_03_08_212306_create_driver_profiles_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('driver_profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('license_number');
+            $table->string('license_plate_number');
+            $table->string('license_photo_front');
+            $table->string('license_photo_back');
+            $table->date('date_of_birth');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
