6d18016a82ebbc1b648ae959f8b8b7d4d3ac4d5e
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-09 21:31:29 -0600

    Driver factory

--- database/factories/DriverProfileFactory.php
+++ database/factories/DriverProfileFactory.php
@@ -2,13 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\DriverProfile;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DriverProfile>
  */
 class DriverProfileFactory extends Factory
 {
+    protected $model = DriverProfile::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,14 @@ class DriverProfileFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'license_number'       => $this->faker->randomElement([
+                $this->faker->numerify('#-####-####'),
+                $this->faker->numerify('############'),
+            ]),
+            'license_plate_number' => strtoupper($this->faker->bothify('???-###')),
+            'license_photo_front'  => $this->faker->imageUrl(640, 480, 'transport', true),
+            'license_photo_back'   => $this->faker->imageUrl(640, 480, 'transport', true),
+            'date_of_birth'        => $this->faker->date('Y-m-d', '-18 years'),
         ];
     }
 }
