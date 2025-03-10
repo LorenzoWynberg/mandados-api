fa894ab4f3e90c86fe4fa4260dd8c3178c729ed0
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-09 21:30:03 -0600

    Driver policy

--- app/Policies/DriverProfilePolicy.php
+++ app/Policies/DriverProfilePolicy.php
@@ -2,65 +2,47 @@
 
 namespace App\Policies;
 
-use App\Models\DriverProfile;
 use App\Models\User;
+use App\Models\DriverProfile;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class DriverProfilePolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
+    use HandlesAuthorization;
+
     public function viewAny(User $user): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
     public function view(User $user, DriverProfile $driverProfile): bool
     {
-        return false;
+        return $user->id === $driverProfile->user_id || $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, DriverProfile $driverProfile): bool
     {
-        return false;
+        return $user->id === $driverProfile->user_id || $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, DriverProfile $driverProfile): bool
     {
-        return false;
+        return $user->id === $driverProfile->user_id || $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
     public function restore(User $user, DriverProfile $driverProfile): bool
     {
-        return false;
+        return $user->id === $driverProfile->user_id || $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
     public function forceDelete(User $user, DriverProfile $driverProfile): bool
     {
-        return false;
+        return $user->hasRole('admin');
     }
 }
