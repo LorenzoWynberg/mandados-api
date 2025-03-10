68e7218ec1d70efacc4c845057ac065a395a1f17
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-09 21:31:45 -0600

    Driver request handlers

--- app/Http/Requests/StoreDriverProfileRequest.php
+++ app/Http/Requests/StoreDriverProfileRequest.php
@@ -11,7 +11,7 @@ class StoreDriverProfileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,25 @@ class StoreDriverProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+            'phone' => 'required|string',
+            'avatar' => 'required|url',
+            'sex' => 'required|in:male,female,unspecified',
+            'license_number' => [
+                'required',
+                'string',
+                'regex:/^(\d-?\d{4}-?\d{4}|\d{12})$/'
+            ],
+            'license_plate_number' => [
+                'required',
+                'string',
+                'regex:/^[A-Z0-9]{3}-\d{3}$/i'
+            ],
+            'license_photo_front' => 'required|url',
+            'license_photo_back' => 'required|url',
+            'date_of_birth' => 'required|date|before:today',
         ];
     }
 }
--- app/Http/Requests/UpdateDriverProfileRequest.php
+++ app/Http/Requests/UpdateDriverProfileRequest.php
@@ -6,23 +6,36 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateDriverProfileRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        $driverProfile = DriverProfile::find($this->route('driver_profile'));
+        return $driverProfile && $this->user()->can('update', $driverProfile);
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|email|unique:users,email,' . $this->route('driver_profile')->id,
+            'password' => 'sometimes|required|string|min:6',
+            'phone' => 'sometimes|required|string',
+            'avatar' => 'sometimes|required|url',
+            'sex' => 'sometimes|required|in:male,female,unspecified',
+            'license_number' => [
+                'sometimes',
+                'required',
+                'string',
+                'regex:/^(\d-?\d{4}-?\d{4}|\d{12})$/'
+            ],
+            'license_plate_number' => [
+                'sometimes',
+                'required',
+                'string',
+                'regex:/^[A-Z0-9]{3}-\d{3}$/i'
+            ],
+            'license_photo_front' => 'sometimes|required|url',
+            'license_photo_back' => 'sometimes|required|url',
+            'date_of_birth' => 'sometimes|required|date|before:today',
         ];
     }
 }
