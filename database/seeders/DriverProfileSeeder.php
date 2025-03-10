29a6d832b7c514bc2177f0001f3a15ecb11142f0
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-09 21:31:16 -0600

    Driver seeder

--- database/seeders/DriverProfileSeeder.php
+++ database/seeders/DriverProfileSeeder.php
@@ -12,6 +12,9 @@ class DriverProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Create driver profiles for 5 users.
+        User::factory()->count(5)->create()->each(function ($user) {
+            DriverProfile::factory()->create(['user_id' => $user->id]);
+        });
     }
 }
