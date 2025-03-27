e9d76f17b1dae28a4aa581ab8394b78e8393f0d5
Lorenzo Wynberg <dev.wynberg@gmail.com>
2025-03-27 09:24:03 -0600

    Additional tests

--- tests/Feature/LocaleMiddlewareTest.php
+++ tests/Feature/LocaleMiddlewareTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Database\Seeders\CatalogSeeder;
 use Tests\TestCase;
@@ -18,9 +19,19 @@ class LocaleMiddlewareTest extends TestCase
 
     public function test_default_locale_is_spanish(): void
     {
+        //        $response = $this->withHeaders([
+        //            'Accept-Language' => ''
+        //        ])->getJson('/api/get-locale');
+        $response = $this->getJson('/api/get/locale');
+        $response->assertOk();
+        $response->assertJson(['locale' => 'es']);
+    }
+
+    public function test_invalid_language_header_falls_back_to_spanish(): void
+    {
         $response = $this->withHeaders([
-            'Accept-Language' => ''
-        ])->getJson('/api/get-locale');
+            'Accept-Language' => 'de', // German not in catalog
+        ])->getJson('/api/get/locale');
 
         $response->assertOk();
         $response->assertJson(['locale' => 'es']);
@@ -30,9 +41,46 @@ class LocaleMiddlewareTest extends TestCase
     {
         $response = $this->withHeaders([
             'Accept-Language' => 'fr',
+        ])->getJson('/api/get/locale');
+
+        $response->assertOk();
+        $response->assertJson(['locale' => 'fr']);
+    }
+
+    public function test_authenticated_user_lang_code_takes_precedence(): void
+    {
+        $user = User::factory()->create(['lang_code' => 'fr']);
+        $this->actingAs($user);
+
+        $response = $this->withHeaders([
+            'Accept-Language' => 'es',
         ])->getJson('/api/get-locale');
 
         $response->assertOk();
         $response->assertJson(['locale' => 'fr']);
+    }
+
+    public function test_authenticated_user_without_lang_code_uses_header(): void
+    {
+        $user = User::factory()->create(['lang_code' => null]);
+        $this->actingAs($user);
+
+        $response = $this->withHeaders([
+            'Accept-Language' => 'fr',
+        ])->getJson('/api/get-locale');
+
+        $response->assertOk();
+        $response->assertJson(['locale' => 'fr']);
+    }
+
+    public function test_authenticated_user_with_invalid_lang_code_falls_back(): void
+    {
+        $user = User::factory()->create(['lang_code' => 'ru']);
+        $this->actingAs($user);
+
+        $response = $this->getJson('/api/get-locale');
+
+        $response->assertOk();
+        $response->assertJson(['locale' => 'es']);
     }
 }
